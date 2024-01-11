32a4bac7baa772abeb08d15457117b86caf6d8d2
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-11 14:22:29 +0100

    101-safely_get_value.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,6 +5,7 @@ from typing import Iterable, List, Sequence, Tuple, Union, Any
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ test file"""
     if lst:
         return lst[0]
     else:
