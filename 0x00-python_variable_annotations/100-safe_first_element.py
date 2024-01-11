c221f2e676d055fd4eda80c4ba92427bd35da265
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-11 14:10:41 +0100

    100-safe_first_element.py

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Task 9's module.
+'''
+from typing import Iterable, List, Sequence, Tuple, Union, Any
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
