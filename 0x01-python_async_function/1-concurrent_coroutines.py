9375c9e4e9fa75896edffb197458f168ffbad5c5
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-15 13:38:12 +0100

    0-basic_async_syntax.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,11 +4,12 @@
 ascending order
 without using sort() because of concurrency."""
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ task 1 pythonasync project"""
     all_list = [await wait_random(max_delay) for i in range(n)]
     return sorted(all_list)
