e6c90e39d5ff7c352590b1d33bf32a9e97c1d668
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-15 13:36:13 +0100

    0-basic_async_syntax.py

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" wait_n should return the list of all the delays
+(float values). The list of the delays should be in
+ascending order
+without using sort() because of concurrency."""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """ task 1 pythonasync project"""
+    all_list = [await wait_random(max_delay) for i in range(n)]
+    return sorted(all_list)
