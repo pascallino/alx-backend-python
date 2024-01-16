4af627c7a3ed47c182c7950d08389712c8baf4b1
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-16 07:32:31 +0100

    1-async_comprehension.py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Write a coroutine called async_generator
+that takes no arguments."""
+
+import asyncio
+from typing import List
+import time
+import random
+
+
+async def async_generator() -> float:
+    """ Write a coroutine called async_generator
+    that takes no arguments."""
+    i = 0
+    while (i < 10):
+        yield random.uniform(0, 10)
+        i = i + 1
+        await asyncio.sleep(1)
---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Import async_generator from the previous
+task and then write a coroutine
+called async_comprehension
+that takes no arguments."""
+
+import asyncio
+from typing import List
+import time
+import random
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """The coroutine will collect 10 random numbers
+    using an async comprehensing
+    over async_generator,
+    then return the 10 random numbers. """
+    g = [i async for i in async_generator()]
+    return g
